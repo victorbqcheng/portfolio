d15ba59fe12b1f966feab384f2a4435d1ed33d77
victor <victoratnz@outlook.com>
2024-09-25 18:29:18 +1200

    Refactor Grid and BentoGridItem components

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -6,10 +6,18 @@ import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <section>
+    <section id='about'>
         <BentoGrid>
             {gridItems.map((item, i)=>(
-                <BentoGridItem key={i} id={item.id} title={item.title} description={item.description} />
+                <BentoGridItem key={i} 
+                    id={item.id} 
+                    title={item.title} 
+                    description={item.description} 
+                    className={item.className} 
+                    img={item.img}
+                    imgClassName={item.imgClassName}
+                    titleClassName={item.titleClassName}
+                    spareImg={item.spareImg} />
             ))}
         </BentoGrid>
     </section>
