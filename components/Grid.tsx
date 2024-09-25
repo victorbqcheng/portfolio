6f8d56e47f336cbb454a7656fc6f248a8b1d825c
victor <victoratnz@outlook.com>
2024-09-25 18:04:29 +1200

    Add GridItem type and data for grid layout

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import MagicButton from './ui/MagicButton'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
-type Item = {
-    title: string;
-    description: string;
-};
-
-const items:Item[] = [
-    {title: 'Item 1', description: 'This is item 1'},
-];
 
 const Grid = () => {
   return (
     <section>
         <BentoGrid>
-            {items.map((item, i)=>(
-                <BentoGridItem key={i} title={item.title} description={item.description} />
+            {gridItems.map((item, i)=>(
+                <BentoGridItem key={i} id={item.id} title={item.title} description={item.description} />
             ))}
         </BentoGrid>
     </section>
